bc882edb2756071bcfc698465758c66f4a9f8fe5
Si Jobling <simon.jobling@gmail.com>
2015-12-08 18:43:30 +0000

    Add Description property to CalendarEvent class

--- src/AddToCal.Logic/CalendarEvent.cs
+++ src/AddToCal.Logic/CalendarEvent.cs
@@ -12,6 +12,7 @@ namespace AddToCal.Logic
         public string Location { get; internal set; }
         public DateTime Start { get; internal set; }
         public string Summary { get; internal set; }
+        public string Description { get; internal set; }
         public string Url { get; internal set; }
     }
 }
